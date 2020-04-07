588a70014c85717fd9a472714ebcff30c2abbee6
naiya891 <naiya891@gmail.com>
2020-04-07 16:07:17 +0900

    4/7午前

--- nbayes2.py
+++ nbayes2.py
@@ -1,7 +1,7 @@
 import numpy as np
 from abc import ABCMeta, abstractmethod
 
-__all__ = ['BaseBinaryNaiveBayes', 'NaiveBayes1']
+__all__ = ['BaseBinaryNaiveBayes', 'NaiveBayes1', 'NaiveBayes2']
 
 class BaseBinaryNaiveBayes(object, metaclass=ABCMeta):
 
@@ -33,13 +33,61 @@ class BaseBinaryNaiveBayes(object, metaclass=ABCMeta):
         
         return y
 
-
 class NaiveBayes1(BaseBinaryNaiveBayes):
     """
     Naive Bayes class (1)
     """
     def __init__(self):
         super(NaiveBayes1, self).__init__()
+
+    def fit(self, X, y):
+
+        #Xの行数(訓練事例数)を代入
+        n_samples = X.shape[0]
+        #Xの列数(特微数)を代入
+        n_features = X.shape[1]
+
+        #クラス数
+        n_classes = 2
+        #特微数
+        n_fvalues = 2
+
+        #特微数とクラスラベル事例数が一致しているか確認
+        if n_samples != len(y):
+            raise ValueError('Mismatched number of samples.')
+
+
+    #クラス分布
+        #nYは各事例を数える箱
+        #nYの初期宣言(大きさはクラスの数)
+        nY = np.zeros(n_classes, dtype=int)
+        for i in range(n_samples):
+            nY[y[i]] += 1
+
+        #各クラスの確率を計算してpYに格納
+        self.pY_ = np.empty(n_classes, dtype=float)
+        for i in range(n_classes):
+            self.pY_[i] = nY[i] / n_samples
+
+    #特徴分布
+        nXY = np.zeros((n_features, n_fvalues, n_classes), dtype=int)
+        for i in range(n_samples):
+            for j in range(n_features):
+                nXY[j, X[i, j], y[i]] += 1
+
+        self.pXgY_ = np.empty((n_features, n_fvalues, n_classes),dtype=float)
+        for j in range(n_features):
+            for xi in range(n_fvalues):
+                for yi in range(n_classes):
+                    self.pXgY_[j, xi, yi] = nXY[j, xi, yi] / float(nY[yi])
+        pass
+
+class NaiveBayes2(BaseBinaryNaiveBayes):
+    """
+    Naive Bayes class (2)
+    """
+    def __init__(self):
+        super(NaiveBayes2, self).__init__()
 
     #X:行数が訓練事例数,列数が特微数からなる2次元配列
     #y:クラスラベル集合の１次元配列
@@ -58,6 +106,7 @@ class NaiveBayes1(BaseBinaryNaiveBayes):
         #特微数とクラスラベル事例数が一致しているか確認
         if n_samples != len(y):
             raise ValueError('Mismatched number of samples.')
+
 
     #クラス分布
         #nYは各事例を数える箱
